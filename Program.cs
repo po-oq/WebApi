a1352f50d567b24bb0de8413c302e8cbe2ced5f2
watanabe <osamu-w@jsmnet.co.jp>
2023-04-27 17:33:57 +0900

    データベースコンテキストの追加

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+// 依存関係の挿入
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +18,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // アプリケーションの開発中にエラーが発生した場合に、開発者にエラーの詳細情報を提供する
+    // 本番環境では無効にすることが推奨
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ Readme.md
@@ -0,0 +1,1 @@
+[チュートリアル: ASP.NET Core で Web API を作成する | Microsoft Learn](https://learn.microsoft.com/ja-jp/aspnet/core/tutorials/first-web-api?view=aspnetcore-6.0&tabs=visual-studio)
